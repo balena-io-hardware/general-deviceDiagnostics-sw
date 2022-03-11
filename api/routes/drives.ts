7393d58e3004f73e3c8cf19898ffa554deb3b5d3
Peter Makra <6892971+mcraa@users.noreply.github.com>
2022-03-11 22:54:16 +0100

    fix(drives): sdk result path

--- api/routes/drives.ts
+++ api/routes/drives.ts
@@ -200,7 +200,7 @@ router.get('/cancel', (req: Request, res: Response) => {
 
 router.get('/sdk/last', async (_: Request, res: Response) => {
   try {
-    const data = fs.readFileSync(path.join(__dirname, 'last_sdk_result.json'), 'utf8')
+    const data = fs.readFileSync(path.join(__dirname, '..', 'last_sdk_result.json'), 'utf8')
     res.json(JSON.parse(data))
   } catch (err) {
     console.error(err)
